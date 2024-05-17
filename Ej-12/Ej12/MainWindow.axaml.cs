466cdd9815722d02fb1c726b149864d0ca097057
samaitor <al.aitor.gallego.garcia@iesportada.org>
2024-05-17 18:55:09 +0200

    message

---
+++ Ej-12/Ej12/MainWindow.axaml.cs
@@ -0,0 +1,204 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Avalonia.Controls;
+using Avalonia.VisualTree;
+using Avalonia.Interactivity;
+using Avalonia.Media;
+namespace Ej12;
+
+public partial class MainWindow : Window
+{
+    List<int> _randomSudoku = [];
+    private readonly List<int> _sudoku = [5,3,4,6,7,2,1,9,8,
+                                          6,7,8,1,9,5,3,4,2,
+                                          9,1,2,3,4,8,5,6,7,
+                                          8,5,9,4,2,6,7,1,3,
+                                          7,6,1,8,5,3,9,2,4,
+                                          4,2,3,7,9,1,8,5,6,
+                                          9,6,1,2,8,7,3,4,5,
+                                          5,3,7,4,1,9,2,8,6,
+                                          2,8,4,6,3,5,1,7,9];
+    
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+     //entrada
+    private void TextBox_OnTextChanged(object? sender, TextChangedEventArgs e)
+    {
+        if (sender is not TextBox tb) return;
+        if (!int.TryParse(tb.Text, out var nVal) || nVal == 0)
+            tb.Text = "";
+    }
+
+    //dificultad sudoku
+    private void Facil_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Reset_Sudoku();
+        MessageLbl.Content = "NIVEL FÁCIL";
+        ComprobarBtn.IsEnabled = true;        
+        
+        foreach (var t in _sudoku)
+        {
+            Random rnd = new();
+            if (rnd.Next(0, 3) == 0)
+                _randomSudoku.Add(0);
+            else
+                _randomSudoku.Add(t);
+        }
+
+        var i = 0;
+        foreach (var tBox in Main.GetVisualDescendants().OfType<TextBox>())
+        {
+            if (_randomSudoku[i] == 0)
+            {
+                tBox.Text = "";
+                tBox.IsReadOnly = false;
+                tBox.Focusable = true;
+                tBox.Foreground = Brushes.Black;
+            }
+            else
+            {
+                tBox.Text = _randomSudoku[i].ToString();
+                tBox.IsReadOnly = true;
+                tBox.Focusable = false;
+                tBox.Foreground = Brushes.DarkSlateBlue;
+            }
+            i++;
+        }
+    }
+    
+    private void Medio_OnClick(object? sender, RoutedEventArgs e)
+    {
+        ComprobarBtn.IsEnabled = true;
+        Reset_Sudoku();
+        MessageLbl.Content = "NIVEL MEDIO";
+        ComprobarBtn.IsEnabled = true;        
+
+        foreach (var t in _sudoku)
+        {
+            Random rnd = new();
+            if (rnd.Next(0, 2) == 0)
+                _randomSudoku.Add(0);
+            else
+                _randomSudoku.Add(t);
+        }
+
+        var i = 0;
+        foreach (var tBox in Main.GetVisualDescendants().OfType<TextBox>())
+        {
+            if (_randomSudoku[i] == 0)
+            {
+                tBox.Text = "";
+                tBox.IsReadOnly = false;
+                tBox.Focusable = true;
+                tBox.Foreground = Brushes.Black;
+            }
+            else
+            {
+                tBox.Text = _randomSudoku[i].ToString();
+                tBox.IsReadOnly = true;
+                tBox.Focusable = false;
+                tBox.Foreground = Brushes.DarkSlateBlue;
+            }
+            i++;
+        }
+    }
+    
+    private void Dificil_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Reset_Sudoku();
+        MessageLbl.Content = "NIVEL DIFÍCIL";
+        ComprobarBtn.IsEnabled = true;        
+
+        foreach (var t in _sudoku)
+        {
+            Random rnd = new();
+            if (rnd.Next(0, 5) == 0)
+                _randomSudoku.Add(t);
+            else
+                _randomSudoku.Add(0);
+        }
+
+        var i = 0;
+        foreach (var tBox in Main.GetVisualDescendants().OfType<TextBox>())
+        {
+            if (_randomSudoku[i] == 0)
+            {
+                tBox.Text = "";
+                tBox.IsReadOnly = false;
+                tBox.Focusable = true;
+                tBox.Foreground = Brushes.Black;
+            }
+            else
+            {
+                tBox.Text = _randomSudoku[i].ToString();
+                tBox.IsReadOnly = true;
+                tBox.Focusable = false;
+                tBox.Foreground = Brushes.DarkSlateBlue;
+            }
+            i++;
+        }
+    }
+    
+    //comprobar
+    private void Comprobar_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var sudokuCorrecto = true;
+        var sudokuCompleto = true;
+        var sudokuIncorrecto = false;
+
+        for (int i = 0; i < _sudoku.Count; i++)
+        {
+            var tBox = Main.GetVisualDescendants().OfType<TextBox>().ElementAt(i);
+            var valorSudoku = _sudoku[i];
+
+            if (tBox.Text != valorSudoku.ToString() && tBox.Text != "")
+            {
+                sudokuCorrecto = false;
+                sudokuIncorrecto = true;
+                sudokuCompleto = false;
+                tBox.Foreground = Brushes.DarkRed;
+            }
+            else if (tBox.Text == "")
+            {
+                sudokuCorrecto = false;
+                sudokuCompleto = false;
+                tBox.Foreground = Brushes.Black;
+            }
+            else if (!tBox.IsReadOnly)
+            {
+                tBox.Foreground = Brushes.Green;
+                tBox.Focusable = false;
+                tBox.IsReadOnly = true;
+            }
+        }
+
+        if (sudokuIncorrecto)
+        {
+            MessageBrd.Background = Brushes.DarkRed;
+            MessageLbl.Content = "SUDOKU INCORRECTO";
+        }
+        else if (!sudokuCompleto)
+        {
+            MessageBrd.Background = Brushes.DarkMagenta;
+            MessageLbl.Content = "SUDOKU INCOMPLETO";
+        }
+        else if (sudokuCorrecto)
+        {
+            MessageBrd.Background = Brushes.DarkGreen;
+            MessageLbl.Content = "SUDOKU CORRECTO";
+        }
+    }
+
+    
+    //resetear sudoku
+    private void Reset_Sudoku()
+    {
+        MessageLbl.Content = "SELECCIONE UNA DIFICULTAD";
+        MessageBrd.Background = Brushes.DarkSlateBlue;
+        _randomSudoku.Clear();
+    }
+}
